3874677314ff5e6544097da4f2230641b893d8ce
Ocampo <mariaocampo0513@gmail.com>
2021-01-01 15:55:21 -0800

    Added server, and routes files. Additionally, I imported the express and fs libraries and created get requests.

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+
+module.exports = function(app) {
+//   * GET `/notes` - Should return the `notes.html` file.
+app.get("/notes", function(req,res){
+    res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
+})
+
+//   * GET `*` - Should return the `index.html` file
+app.get("*", function(req,res){
+    res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
+})
+}
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require ("express");
+// const apiRoutes = require ("./routes/apiRoutes");
+const app = express();
+// const htmlRoutes = require ("./routes/htmlRoutes");
+
+
+const PORT = process.env.PORT || 8000;
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+// app.use("/api", apiRoutes);
+// app.use("/", htmlRoutes);
+
+require ("./routes/apiRoutes")(app)
+require ("./routes/htmlRoutes")(app)
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}...`));
