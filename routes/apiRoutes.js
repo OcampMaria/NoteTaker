8fc0876ee672f0db4785198a648da3cc6bbb63a5
Ocampo <mariaocampo0513@gmail.com>
2021-01-02 13:30:08 -0800

    changed le paths

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
-const util = require('util');
+const fs = require("fs");
+const util = require("util");
 
 const readFileasync = util.promisify(fs.readFile);
 
 module.exports = function (app) {
 
-    app.get("/api/notes", function (req, res) {
-    readFileasync("../Develop/db/db.json", "utf8").then(console.log(data))
+    app.get("/api/notes", function(req, res) {
+    readFileasync("./db/db.json", "utf8").then(data => console.log(data))
     });
 
     app.post("/api/notes", function(req, res) {
