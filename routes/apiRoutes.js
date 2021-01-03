7166bbf5f9ad391e5fb084ef48e68928626215b1
Ocampo <mariaocampo0513@gmail.com>
2021-01-02 19:55:40 -0800

    .

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,33 @@
+const { notStrictEqual } = require("assert");
 const fs = require("fs");
 const util = require("util");
-
 const readFileasync = util.promisify(fs.readFile);
+
 
 module.exports = function (app) {
 
     app.get("/api/notes", function(req, res) {
-    readFileasync("./db/db.json", "utf8").then(data => console.log(data))
+        readFileasync("./db/db.json", "utf8").then(data =>{
+        res.send(data);
+        console.log(data);
+    })
     });
 
     app.post("/api/notes", function(req, res) {
-       
-    });
+        readFileasync("./db/db.json", "utf8").then(data =>{
+            console.log(data, "push");
 
+            const newNote = {
+                title:req.body.title,
+                text:req.body.name
+            };
+            res.json(newNote)
+            data.push(newNote);
+            res.send(newNote);
+        })
+        
+    });
+   
     app.delete("/api/notes/:id", function(req,res){
 
     })
